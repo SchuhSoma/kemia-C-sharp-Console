a603c94ced1463d6f220a986a00b04bc02b216ee
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-03-10 19:30:06 +0100

    Add files via upload

---
+++ kemia/kemia/Kemia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kemia
+{
+    class Kemia
+    {
+        //Év;Elem;Vegyjel;Rendszám;Felfedező
+        public string Ev;
+        public string Elem;
+        public string Vegyjel;
+        public double Rendszam;
+        public string Felfedezo;
+
+        public Kemia (string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Ev = dbok[0];
+            this.Elem = dbok[1];
+            this.Vegyjel = dbok[2];
+            this.Rendszam = double.Parse(dbok[3]);
+            this.Felfedezo = dbok[4];
+        }
+    }
+}
